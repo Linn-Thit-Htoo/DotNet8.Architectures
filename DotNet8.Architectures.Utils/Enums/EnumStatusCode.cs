feafc694e6abef26a662852ac611f87ba0a0cf1d
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 00:16:57 +0630

    -

--- DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
+++ DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.Architectures.Utils.Enums
+﻿namespace DotNet8.Architectures.Utils.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+    None,
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
