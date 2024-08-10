459adca412edb0b01bee60ee861e6a16f7dd0018
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 00:16:50 +0630

    -

--- DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
+++ DotNet8.Architectures.Utils/Enums/EnumStatusCode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.Utils.Enums
+﻿namespace DotNet8.Architectures.Utils.Enums
 {
     public enum EnumStatusCode
     {
