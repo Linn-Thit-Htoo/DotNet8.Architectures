6b69c4271b2fb337126206561785ca05eb7b5976
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 00:16:19 +0630

    -

--- DotNet8.Architectures.DTOs/Features/Blog/BlogDTO.cs
+++ DotNet8.Architectures.DTOs/Features/Blog/BlogDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.Architectures.DTOs.Features.Blog
+﻿namespace DotNet8.Architectures.DTOs.Features.Blog
 {
     public class BlogDto
     {
