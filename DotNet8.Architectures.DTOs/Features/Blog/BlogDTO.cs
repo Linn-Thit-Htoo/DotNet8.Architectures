868a676c1fc4511b31d03df576cf3919e3270466
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 00:16:29 +0630

    -

--- DotNet8.Architectures.DTOs/Features/Blog/BlogDTO.cs
+++ DotNet8.Architectures.DTOs/Features/Blog/BlogDTO.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.Architectures.DTOs.Features.Blog
+﻿namespace DotNet8.Architectures.DTOs.Features.Blog;
+
+public class BlogDto
 {
-    public class BlogDto
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
