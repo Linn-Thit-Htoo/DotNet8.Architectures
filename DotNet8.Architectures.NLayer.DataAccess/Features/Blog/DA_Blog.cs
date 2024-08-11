aa2c48def5fe0da34d0746507e6d2e6178ee602c
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:45:04 +0630

    Format Code

--- DotNet8.Architectures.NLayer.DataAccess/Features/Blog/DA_Blog.cs
+++ DotNet8.Architectures.NLayer.DataAccess/Features/Blog/DA_Blog.cs
@@ -5,42 +5,41 @@ using DotNet8.Architectures.Extensions;
 using DotNet8.Architectures.Utils;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet8.Architectures.NLayer.DataAccess.Features.Blog
+namespace DotNet8.Architectures.NLayer.DataAccess.Features.Blog;
+
+public class DA_Blog
 {
-    public class DA_Blog
+    private readonly AppDbContext _context;
+
+    public DA_Blog(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public DA_Blog(AppDbContext context)
+    public async Task<Result<BlogListDto>> GetBlogs(int pageNo, int pageSize)
+    {
+        Result<BlogListDto> result;
+        try
         {
-            _context = context;
-        }
+            var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
+            var lst = await query.Skip((pageNo - 1) * pageSize).Take(pageSize).ToListAsync();
 
-        public async Task<Result<BlogListDto>> GetBlogs(int pageNo, int pageSize)
-        {
-            Result<BlogListDto> result;
-            try
+            var totalCount = await query.CountAsync();
+            var pageCount = totalCount / pageSize;
+
+            if (totalCount % pageSize > 0)
             {
-                var query = _context.Tbl_Blogs.OrderByDescending(x => x.BlogId);
-                var lst = await query.Skip((pageNo - 1) * pageSize).Take(pageSize).ToListAsync();
-
-                var totalCount = await query.CountAsync();
-                var pageCount = totalCount / pageSize;
-
-                if (totalCount % pageSize > 0)
-                {
-                    pageCount++;
-                }
-
-                var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
-                result = Result<BlogListDto>.Success(new BlogListDto { DataLst = lst.Select(x => x.ToDto()), PageSetting = pageSettingModel });
-            }
-            catch (Exception ex)
-            {
-                result = Result<BlogListDto>.Failure(ex);
+                pageCount++;
             }
 
-            return result;
+            var pageSettingModel = new PageSettingModel(pageNo, pageSize, pageCount, totalCount);
+            result = Result<BlogListDto>.Success(new BlogListDto { DataLst = lst.Select(x => x.ToDto()), PageSetting = pageSettingModel });
         }
+        catch (Exception ex)
+        {
+            result = Result<BlogListDto>.Failure(ex);
+        }
+
+        return result;
     }
 }
