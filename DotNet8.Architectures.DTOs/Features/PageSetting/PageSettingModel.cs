d353b083bb412a4f76d68a3e8bd73f0149f003d5
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:48:14 +0630

    Format Code

--- DotNet8.Architectures.DTOs/Features/PageSetting/PageSettingModel.cs
+++ DotNet8.Architectures.DTOs/Features/PageSetting/PageSettingModel.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.Architectures.DTOs.Features.PageSetting;
 
-namespace DotNet8.Architectures.DTOs.Features.PageSetting
+public class PageSettingModel
 {
-    public class PageSettingModel
-    {
-        public int PageNo { get; set; }
-        public int PageSize { get; set; }
-        public int PageCount { get; set; }
-        public int TotalCount { get; set; }
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public int TotalCount { get; set; }
 
-        public PageSettingModel(int pageNo, int pageSize, int pageCount, int totalCount)
-        {
-            PageNo = pageNo;
-            PageSize = pageSize;
-            PageCount = pageCount;
-            TotalCount = totalCount;
-        }
+    public PageSettingModel(int pageNo, int pageSize, int pageCount, int totalCount)
+    {
+        PageNo = pageNo;
+        PageSize = pageSize;
+        PageCount = pageCount;
+        TotalCount = totalCount;
     }
 }
