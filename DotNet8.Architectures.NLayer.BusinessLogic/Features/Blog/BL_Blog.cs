acab73688ff6d3e7818a0ae1479b6590ec00a336
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:43:32 +0630

    -

--- DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
+++ DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
@@ -2,11 +2,6 @@
 using DotNet8.Architectures.NLayer.DataAccess.Features.Blog;
 using DotNet8.Architectures.Utils;
 using DotNet8.Architectures.Utils.Resources;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.NLayer.BusinessLogic.Features.Blog
 {
