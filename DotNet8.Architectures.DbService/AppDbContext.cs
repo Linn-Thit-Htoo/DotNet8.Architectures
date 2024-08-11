567308cf8c0047d1ef860b99d748864c2c4b44e0
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:47:34 +0630

    Format Code

--- DotNet8.Architectures.DbService/AppDbContext.cs
+++ DotNet8.Architectures.DbService/AppDbContext.cs
@@ -1,10 +1,5 @@
 ﻿using DotNet8.Architectures.DbService.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.DbService
 {
