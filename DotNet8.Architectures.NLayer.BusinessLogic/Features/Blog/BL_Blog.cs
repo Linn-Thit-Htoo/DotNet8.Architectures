ccba6575ebf1bca084edd38008be31a6ff9aadf1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:43:44 +0630

    Format Coede

--- DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
+++ DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
@@ -3,37 +3,36 @@ using DotNet8.Architectures.NLayer.DataAccess.Features.Blog;
 using DotNet8.Architectures.Utils;
 using DotNet8.Architectures.Utils.Resources;
 
-namespace DotNet8.Architectures.NLayer.BusinessLogic.Features.Blog
+namespace DotNet8.Architectures.NLayer.BusinessLogic.Features.Blog;
+
+public class BL_Blog
 {
-    public class BL_Blog
+    private readonly DA_Blog _dA_Blog;
+
+    public BL_Blog(DA_Blog dA_Blog)
     {
-        private readonly DA_Blog _dA_Blog;
+        _dA_Blog = dA_Blog;
+    }
 
-        public BL_Blog(DA_Blog dA_Blog)
+    public async Task<Result<BlogListDto>> GetBlogs(int pageNo, int pageSize)
+    {
+        Result<BlogListDto> result;
+
+        if (pageNo <= 0)
         {
-            _dA_Blog = dA_Blog;
+            result = Result<BlogListDto>.Failure(MessageResource.InvalidPageNo);
+            goto result;
         }
 
-        public async Task<Result<BlogListDto>> GetBlogs(int pageNo, int pageSize)
+        if (pageSize <= 0)
         {
-            Result<BlogListDto> result;
-
-            if (pageNo <= 0)
-            {
-                result = Result<BlogListDto>.Failure(MessageResource.InvalidPageNo);
-                goto result;
-            }
-
-            if (pageSize <= 0)
-            {
-                result = Result<BlogListDto>.Failure(MessageResource.InvalidPageSize);
-                goto result;
-            }
-
-            result = await _dA_Blog.GetBlogs(pageNo, pageSize);
-
-        result:
-            return result;
+            result = Result<BlogListDto>.Failure(MessageResource.InvalidPageSize);
+            goto result;
         }
+
+        result = await _dA_Blog.GetBlogs(pageNo, pageSize);
+
+    result:
+        return result;
     }
 }
