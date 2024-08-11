42405d2e80156f5117bfb9742627712ee2c66605
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:45:57 +0630

    Format Code

--- DotNet8.Architectures.NLayer.Presentation/Controllers/BaseController.cs
+++ DotNet8.Architectures.NLayer.Presentation/Controllers/BaseController.cs
@@ -1,15 +1,14 @@
 ﻿using DotNet8.Architectures.Shared;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Architectures.NLayer.Presentation.Controllers
+namespace DotNet8.Architectures.NLayer.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
