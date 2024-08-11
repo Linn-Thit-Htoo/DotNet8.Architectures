3f34adcb6fd4cfe9156fd729a82ef86919c24a1b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:45:46 +0630

    Format Code

--- DotNet8.Architectures.NLayer.Presentation/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.NLayer.Presentation/Controllers/Blog/BlogController.cs
@@ -1,24 +1,23 @@
 ﻿using DotNet8.Architectures.NLayer.BusinessLogic.Features.Blog;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.Architectures.NLayer.Presentation.Controllers.Blog
+namespace DotNet8.Architectures.NLayer.Presentation.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
+        _bL_Blog = bL_Blog;
+    }
 
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs(int pageNo, int pageSize)
-        {
-            var result = await _bL_Blog.GetBlogs(pageNo, pageSize);
-            return Content(result);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs(int pageNo, int pageSize)
+    {
+        var result = await _bL_Blog.GetBlogs(pageNo, pageSize);
+        return Content(result);
     }
 }
