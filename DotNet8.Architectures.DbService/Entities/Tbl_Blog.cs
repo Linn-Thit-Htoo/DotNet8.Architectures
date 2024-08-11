c5659255f580bca8aaddb904a7ac608cd4950585
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-11 11:47:15 +0630

    Format Code

--- DotNet8.Architectures.DbService/Entities/Tbl_Blog.cs
+++ DotNet8.Architectures.DbService/Entities/Tbl_Blog.cs
@@ -2,15 +2,14 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.Architectures.DbService.Entities
+namespace DotNet8.Architectures.DbService.Entities;
+
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
