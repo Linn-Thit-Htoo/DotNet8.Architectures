bf972bdfc2ad00a5efa842b3438958502a73f5d7
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-16 20:08:30 +0630

    [ Added ] Validation in BL Add Blog

--- DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
+++ DotNet8.Architectures.NLayer.BusinessLogic/Features/Blog/BL_Blog.cs
@@ -1,5 +1,6 @@
 ﻿using DotNet8.Architectures.DTOs.Features.Blog;
 using DotNet8.Architectures.NLayer.DataAccess.Features.Blog;
+using DotNet8.Architectures.Shared;
 using DotNet8.Architectures.Utils;
 using DotNet8.Architectures.Utils.Resources;
 
@@ -8,10 +9,12 @@ namespace DotNet8.Architectures.NLayer.BusinessLogic.Features.Blog;
 public class BL_Blog
 {
     private readonly DA_Blog _dA_Blog;
+    private readonly BlogValidator _blogValidator;
 
-    public BL_Blog(DA_Blog dA_Blog)
+    public BL_Blog(DA_Blog dA_Blog, BlogValidator blogValidator)
     {
         _dA_Blog = dA_Blog;
+        _blogValidator = blogValidator;
     }
 
     public async Task<Result<BlogListDto>> GetBlogsAsync(int pageNo, int pageSize)
@@ -54,6 +57,19 @@ public class BL_Blog
 
     public async Task<Result<BlogDto>> AddBlogAsync(BlogRequestDto blogDto)
     {
-        return await _dA_Blog.AddBlogAsync(blogDto);
+        Result<BlogDto> result;
+
+        var validationResult = await _blogValidator.ValidateAsync(blogDto);
+        if (!validationResult.IsValid)
+        {
+            string errors = string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage));
+            result = Result<BlogDto>.Failure(errors);
+            goto result;
+        }
+
+        result = await _dA_Blog.AddBlogAsync(blogDto);
+
+    result:
+        return result;
     }
 }
