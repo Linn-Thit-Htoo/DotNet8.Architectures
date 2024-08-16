fb8554dd06da3698294b795b56a10fa777fb8e4f
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-16 20:24:39 +0630

    Format Code

--- DotNet8.Architectures.DTOs/Features/Blog/BlogRequestDTO.cs
+++ DotNet8.Architectures.DTOs/Features/Blog/BlogRequestDTO.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.Architectures.DTOs.Features.Blog;
 
-namespace DotNet8.Architectures.DTOs.Features.Blog
+public class BlogRequestDto
 {
-    public class BlogRequestDto
-    {
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
