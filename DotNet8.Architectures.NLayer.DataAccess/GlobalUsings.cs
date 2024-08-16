632dbff6aa23cc150ba53785f599ce3b6f0fed88
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-16 20:21:04 +0630

    Format Code

---
+++ DotNet8.Architectures.NLayer.DataAccess/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using DotNet8.Architectures.DbService;
+global using DotNet8.Architectures.DTOs.Features.Blog;
+global using DotNet8.Architectures.DTOs.Features.PageSetting;
+global using DotNet8.Architectures.Extensions;
+global using DotNet8.Architectures.Utils;
+global using Microsoft.EntityFrameworkCore;
