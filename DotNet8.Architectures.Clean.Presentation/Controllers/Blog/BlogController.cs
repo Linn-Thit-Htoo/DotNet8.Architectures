d17c327c0d394449eeab24dd2e3fbc26bcca4f6f
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-17 15:53:17 +0630

    [ Added ] GetBlogs Endpoint

---
+++ DotNet8.Architectures.Clean.Presentation/Controllers/Blog/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using DotNet8.Architectures.Clean.Application.Blog.GetBlogList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.Architectures.Clean.Presentation.Controllers.Blog
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public BlogController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs(int pageNo, int pageSize, CancellationToken cancellationToken)
+        {
+            var query = new GetBlogListQuery(pageNo, pageSize);
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Content(result);
+        }
+    }
+}
