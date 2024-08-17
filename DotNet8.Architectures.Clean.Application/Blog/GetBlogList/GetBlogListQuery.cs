17c6429cf10d2a3a82b60622e6b4a1139c41b3c8
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-17 15:39:49 +0630

    [ Added ] GetBlogListQueryHandler

--- DotNet8.Architectures.Clean.Application/Blog/GetBlogList/GetBlogListQuery.cs
+++ DotNet8.Architectures.Clean.Application/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.GetBlogList
 {
-    internal class GetBlogListQuery
+    public class GetBlogListQuery : IRequest<Result<BlogListDtoV1>>
     {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+
+        public GetBlogListQuery(int pageNo, int pageSize)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+        }
     }
 }
--- DotNet8.Architectures.Clean.Application/Blog/GetBlogList/GetBlogListQueryHandler.cs
+++ DotNet8.Architectures.Clean.Application/Blog/GetBlogList/GetBlogListQueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DotNet8.Architectures.Clean.Domain.Blog;
+using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using DotNet8.Architectures.Utils.Resources;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,35 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.GetBlogList
 {
-    internal class GetBlogListQueryHandler
+    public class GetBlogListQueryHandler : IRequestHandler<GetBlogListQuery, Result<BlogListDtoV1>>
     {
+        private readonly IBlogRepository _blogRepository;
+
+        public GetBlogListQueryHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<Result<BlogListDtoV1>> Handle(GetBlogListQuery request, CancellationToken cancellationToken)
+        {
+            Result<BlogListDtoV1> result;
+
+            if (request.PageNo <= 0)
+            {
+                result = Result<BlogListDtoV1>.Failure(MessageResource.InvalidPageNo);
+                goto result;
+            }
+
+            if (request.PageSize <= 0)
+            {
+                result = Result<BlogListDtoV1>.Failure(MessageResource.InvalidPageSize);
+                goto result;
+            }
+
+            result = await _blogRepository.GetBlogsAsync(request.PageNo, request.PageSize, cancellationToken);
+
+        result:
+            return result;
+        }
     }
 }
