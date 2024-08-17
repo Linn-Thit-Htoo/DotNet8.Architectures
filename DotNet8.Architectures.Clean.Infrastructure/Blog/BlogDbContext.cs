7c528692814ad89e3e3d43303d73dead9701f2f1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-17 14:56:51 +0630

    -

---
+++ DotNet8.Architectures.Clean.Infrastructure/Blog/BlogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.Clean.Infrastructure.Blog
+{
+    internal class BlogDbContext
+    {
+    }
+}
---
+++ DotNet8.Architectures.Clean.Presentation/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.Architectures.Clean.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+    }
+}
