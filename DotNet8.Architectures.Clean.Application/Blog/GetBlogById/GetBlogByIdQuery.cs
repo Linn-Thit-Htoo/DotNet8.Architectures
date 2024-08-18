9dbf2bcb8536084be1bb1efe4ef8bde55e7023a4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-18 23:43:26 +0630

    -

--- DotNet8.Architectures.Clean.Application/Blog/GetBlogById/GetBlogByIdQuery.cs
+++ DotNet8.Architectures.Clean.Application/Blog/GetBlogById/GetBlogByIdQuery.cs
@@ -1,11 +1,6 @@
 ﻿using DotNet8.Architectures.DTOs.Features.Blog;
 using DotNet8.Architectures.Utils;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.GetBlogById
 {
