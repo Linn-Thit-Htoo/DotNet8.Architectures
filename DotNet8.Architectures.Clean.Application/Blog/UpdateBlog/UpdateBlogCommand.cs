13dca771d480974a156750984f962f00bc73364f
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-20 01:21:19 +0630

    [ Added ] UpdateBlogCommandHandler

--- DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.UpdateBlog
 {
-    internal class UpdateBlogCommand
+    public class UpdateBlogCommand : IRequest<Result<BlogDto>>
     {
+        public BlogRequestDto RequestDto { get; set; }
+        public int BlogId { get; set; }
+
+        public UpdateBlogCommand(BlogRequestDto requestDto, int blogId)
+        {
+            RequestDto = requestDto;
+            BlogId = blogId;
+        }
     }
 }
--- DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
+++ DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DotNet8.Architectures.Clean.Domain.Blog;
+using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Shared;
+using DotNet8.Architectures.Utils;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,41 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.UpdateBlog
 {
-    internal class UpdateBlogCommandHandler
+    public class UpdateBlogCommandHandler : IRequestHandler<UpdateBlogCommand, Result<BlogDto>>
     {
+        private readonly IBlogRepository _blogRepository;
+
+        public UpdateBlogCommandHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<Result<BlogDto>> Handle(UpdateBlogCommand request, CancellationToken cancellationToken)
+        {
+            Result<BlogDto> result;
+
+            if (request.RequestDto.BlogTitle.IsNullOrEmpty())
+            {
+                result = Result<BlogDto>.Failure("Blog Title cannot be empty.");
+                goto result;
+            }
+
+            if (request.RequestDto.BlogAuthor.IsNullOrEmpty())
+            {
+                result = Result<BlogDto>.Failure("Blog Author cannot be empty.");
+                goto result;
+            }
+
+            if (request.RequestDto.BlogContent.IsNullOrEmpty())
+            {
+                result = Result<BlogDto>.Failure("Blog Content");
+                goto result;
+            }
+
+            result = await _blogRepository.UpdateBlogAsync(request.RequestDto, request.BlogId, cancellationToken);
+
+        result:
+            return result;
+        }
     }
 }
