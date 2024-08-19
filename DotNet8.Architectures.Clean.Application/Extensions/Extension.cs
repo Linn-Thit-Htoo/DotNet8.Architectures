59b82ba0ca7b3606b9a28753f56bf8aa678d8175
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-19 10:47:52 +0630

    -

--- DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace DotNet8.Architectures.Clean.Application.Extensions
+namespace DotNet8.Architectures.Clean.Application.Extensions;
+
+public static class Extension
 {
-    public static class Extension
+    public static IServiceCollection AddMediatRService(this IServiceCollection services)
     {
-        public static IServiceCollection AddMediatRService(this IServiceCollection services)
-        {
-            return services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Extension).Assembly));
-        }
+        return services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Extension).Assembly));
     }
 }
