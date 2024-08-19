24c6d1faefcaeeebc43cb26452ddc850b86fb0cb
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-20 01:26:08 +0630

    Format Code

--- DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommand.cs
+++ DotNet8.Architectures.Clean.Application/Blog/UpdateBlog/UpdateBlogCommand.cs
@@ -1,23 +1,17 @@
 ﻿using DotNet8.Architectures.DTOs.Features.Blog;
 using DotNet8.Architectures.Utils;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8.Architectures.Clean.Application.Blog.UpdateBlog
+namespace DotNet8.Architectures.Clean.Application.Blog.UpdateBlog;
+
+public class UpdateBlogCommand : IRequest<Result<BlogDto>>
 {
-    public class UpdateBlogCommand : IRequest<Result<BlogDto>>
-    {
-        public BlogRequestDto RequestDto { get; set; }
-        public int BlogId { get; set; }
+    public BlogRequestDto RequestDto { get; set; }
+    public int BlogId { get; set; }
 
-        public UpdateBlogCommand(BlogRequestDto requestDto, int blogId)
-        {
-            RequestDto = requestDto;
-            BlogId = blogId;
-        }
+    public UpdateBlogCommand(BlogRequestDto requestDto, int blogId)
+    {
+        RequestDto = requestDto;
+        BlogId = blogId;
     }
 }
