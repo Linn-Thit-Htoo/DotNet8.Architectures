c542a582a04b2a6eb7360773f2f332d167042efb
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-20 01:18:21 +0630

    [ Added ] DeleteBlogCommandHandler

--- DotNet8.Architectures.Clean.Application/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
+++ DotNet8.Architectures.Clean.Application/Blog/DeleteBlog/DeleteBlogCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DotNet8.Architectures.Clean.Domain.Blog;
+using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using DotNet8.Architectures.Utils.Resources;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,29 @@ using System.Threading.Tasks;
 
 namespace DotNet8.Architectures.Clean.Application.Blog.DeleteBlog
 {
-    internal class DeleteBlogCommandHandler
+    public class DeleteBlogCommandHandler : IRequestHandler<DeleteBlogCommand, Result<BlogDto>>
     {
+        private readonly IBlogRepository _blogRepository;
+
+        public DeleteBlogCommandHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<Result<BlogDto>> Handle(DeleteBlogCommand request, CancellationToken cancellationToken)
+        {
+            Result<BlogDto> result;
+
+            if (request.BlogId <= 0)
+            {
+                result = Result<BlogDto>.Failure(MessageResource.InvalidId);
+                goto result;
+            }
+
+            result = await _blogRepository.DeleteBlogAsync(request.BlogId, cancellationToken);
+
+        result:
+            return result;
+        }
     }
 }
