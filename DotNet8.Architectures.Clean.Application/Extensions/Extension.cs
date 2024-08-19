0b11c68393ca0e9d539014d85e4f11588ded45c6
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-19 10:47:58 +0630

    -

--- DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Clean.Application/Extensions/Extension.cs
@@ -6,6 +6,8 @@ public static class Extension
 {
     public static IServiceCollection AddMediatRService(this IServiceCollection services)
     {
-        return services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(Extension).Assembly));
+        return services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(Extension).Assembly)
+        );
     }
 }
