48d9933c7d7ced4c29608d26dd5aad2324b34687
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-19 10:48:36 +0630

    Format Code

--- DotNet8.Architectures.Clean.Domain/Blog/Tbl_Blog.cs
+++ DotNet8.Architectures.Clean.Domain/Blog/Tbl_Blog.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DotNet8.Architectures.Clean.Domain.Blog
+namespace DotNet8.Architectures.Clean.Domain.Blog;
+
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
