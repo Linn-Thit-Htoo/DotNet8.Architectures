9155221b033df7fe1982ede43e6ca80e134b2572
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:03:37 +0630

    [ Added ] Global Usings

--- DotNet8.Architectures.Hexagonal.Domain/Features/Blog/IBlogPort.cs
+++ DotNet8.Architectures.Hexagonal.Domain/Features/Blog/IBlogPort.cs
@@ -1,35 +1,26 @@
-﻿using DotNet8.Architectures.DTOs.Features.Blog;
-using DotNet8.Architectures.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.Architectures.Hexagonal.Domain.Features.Blog;
 
-namespace DotNet8.Architectures.Hexagonal.Domain.Features.Blog
+public interface IBlogPort
 {
-    public interface IBlogPort
-    {
-        Task<Result<BlogListDtoV1>> GetBlogsAsync(
-            int pageNo,
-            int pageSize,
-            CancellationToken cancellationToken
-        );
-        Task<Result<BlogDto>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
-        Task<Result<BlogDto>> AddBlogAsync(
-            BlogRequestDto blogRequest,
-            CancellationToken cancellationToken
-        );
-        Task<Result<BlogDto>> UpdateBlogAsync(
-            BlogRequestDto requestDto,
-            int id,
-            CancellationToken cancellationToken
-        );
-        Task<Result<BlogDto>> PatchBlogAsync(
-            BlogRequestDto requestDto,
-            int id,
-            CancellationToken cancellationToken
-        );
-        Task<Result<BlogDto>> DeleteBlogAsync(int id, CancellationToken cancellationToken);
-    }
+    Task<Result<BlogListDtoV1>> GetBlogsAsync(
+        int pageNo,
+        int pageSize,
+        CancellationToken cancellationToken
+    );
+    Task<Result<BlogDto>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
+    Task<Result<BlogDto>> AddBlogAsync(
+        BlogRequestDto blogRequest,
+        CancellationToken cancellationToken
+    );
+    Task<Result<BlogDto>> UpdateBlogAsync(
+        BlogRequestDto requestDto,
+        int id,
+        CancellationToken cancellationToken
+    );
+    Task<Result<BlogDto>> PatchBlogAsync(
+        BlogRequestDto requestDto,
+        int id,
+        CancellationToken cancellationToken
+    );
+    Task<Result<BlogDto>> DeleteBlogAsync(int id, CancellationToken cancellationToken);
 }
