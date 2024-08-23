7193491a6e456714e3bc00c905ea3257e7b0bb42
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:11:58 +0630

    Format Code

--- DotNet8.Architectures.Microservices.Blog/Features/BaseController.cs
+++ DotNet8.Architectures.Microservices.Blog/Features/BaseController.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿namespace DotNet8.Architectures.Microservices.Blog.Features;
 
-namespace DotNet8.Architectures.Microservices.Blog.Features
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
