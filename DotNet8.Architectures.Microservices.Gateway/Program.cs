21acceb9dd82f1758425caca16fa1f5a3730cde9
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:03:27 +0630

    [ Added ] Ocelot

--- DotNet8.Architectures.Microservices.Gateway/Program.cs
+++ DotNet8.Architectures.Microservices.Gateway/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseOcelot().Wait();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
