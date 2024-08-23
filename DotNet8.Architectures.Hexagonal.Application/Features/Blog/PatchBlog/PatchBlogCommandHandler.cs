e6b19eb60e96f3baba0ffcd2bc4fbfba1f9d4846
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:03:03 +0630

    Format Code

--- DotNet8.Architectures.Hexagonal.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
+++ DotNet8.Architectures.Hexagonal.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.Architectures.Hexagonal.Application.Features.Blog.PatchBlog;
 
-namespace DotNet8.Architectures.Hexagonal.Application.Features.Blog.PatchBlog
+public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogDto>>
 {
-    public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogDto>>
+    private readonly IBlogPort _blogPort;
+
+    public PatchBlogCommandHandler(IBlogPort blogPort)
     {
-        private readonly IBlogPort _blogPort;
+        _blogPort = blogPort;
+    }
 
-        public PatchBlogCommandHandler(IBlogPort blogPort)
+    public async Task<Result<BlogDto>> Handle(
+        PatchBlogCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        Result<BlogDto> result;
+
+        if (request.BlogId <= 0)
         {
-            _blogPort = blogPort;
+            result = Result<BlogDto>.Failure(MessageResource.InvalidId);
+            goto result;
         }
 
-        public async Task<Result<BlogDto>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
-        {
-            Result<BlogDto> result;
+        result = await _blogPort.PatchBlogAsync(
+            request.BlogRequestDto,
+            request.BlogId,
+            cancellationToken
+        );
 
-            if (request.BlogId <= 0)
-            {
-                result = Result<BlogDto>.Failure(MessageResource.InvalidId);
-                goto result;
-            }
-
-            result = await _blogPort.PatchBlogAsync(request.BlogRequestDto, request.BlogId, cancellationToken);
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
