466a7cb7bf7db6c93c4924b3d58c184acc925da4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:00:39 +0630

    Format Code

--- DotNet8.Architectures.Hexagonal.API/GlobalUsings.cs
+++ DotNet8.Architectures.Hexagonal.API/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using DotNet8.Architectures.DbService;
 global using DotNet8.Architectures.DTOs.Features.Blog;
 global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.CreateBlog;
 global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.DeleteBlog;
@@ -7,6 +8,9 @@ global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.GetBlogBy
 global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.GetBlogList;
 global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.PatchBlog;
 global using DotNet8.Architectures.Hexagonal.Application.Features.Blog.UpdateBlog;
+global using DotNet8.Architectures.Hexagonal.Domain.Features.Blog;
+global using DotNet8.Architectures.Hexagonal.Infrastructure.Features.Blog;
 global using DotNet8.Architectures.Shared;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
