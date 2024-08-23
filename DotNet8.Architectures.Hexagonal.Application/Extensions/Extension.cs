665729f53e0bce311942c846ca23080bc1feed09
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:01:27 +0630

    [ Added ] Global Usings

--- DotNet8.Architectures.Hexagonal.Application/Extensions/Extension.cs
+++ DotNet8.Architectures.Hexagonal.Application/Extensions/Extension.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace DotNet8.Architectures.Hexagonal.Application.Extensions;
+﻿namespace DotNet8.Architectures.Hexagonal.Application.Extensions;
 
 public static class Extension
 {
