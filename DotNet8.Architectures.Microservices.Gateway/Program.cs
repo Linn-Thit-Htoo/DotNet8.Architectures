70cc6026a65b0ebde6a3b27c286e1b1b29987c5b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:07:17 +0630

    [ Added ] Global Usings

--- DotNet8.Architectures.Microservices.Gateway/Program.cs
+++ DotNet8.Architectures.Microservices.Gateway/Program.cs
@@ -1,11 +1,9 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
