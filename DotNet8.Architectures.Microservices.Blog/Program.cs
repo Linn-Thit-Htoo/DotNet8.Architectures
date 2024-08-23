54f73dcf471944de6ab2da4fa15f9a3461dd51ee
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:12:33 +0630

    [ Complete ] Formatting Microservices

--- DotNet8.Architectures.Microservices.Blog/Program.cs
+++ DotNet8.Architectures.Microservices.Blog/Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,7 +9,6 @@ builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
