8964fe08343d6f1a9af91ac3fc95ec57530c3483
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:58:56 +0630

    Format Code

--- DotNet8.Architectures.Hexagonal.API/Controllers/Blog/BlogController.cs
+++ DotNet8.Architectures.Hexagonal.API/Controllers/Blog/BlogController.cs
@@ -1,79 +1,82 @@
-﻿namespace DotNet8.Architectures.Hexagonal.API.Controllers.Blog
+﻿namespace DotNet8.Architectures.Hexagonal.API.Controllers.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly IMediator _mediator;
+
+    public BlogController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public BlogController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs(
+        int pageNo,
+        int pageSize,
+        CancellationToken cancellationToken
+    )
+    {
+        var query = new GetBlogListQuery(pageNo, pageSize);
+        var result = await _mediator.Send(query, cancellationToken);
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs(
-            int pageNo,
-            int pageSize,
-            CancellationToken cancellationToken
-        )
-        {
-            var query = new GetBlogListQuery(pageNo, pageSize);
-            var result = await _mediator.Send(query, cancellationToken);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBlogById(int id, CancellationToken cancellationToken)
+    {
+        var query = new GetBlogByIdQuery(id);
+        var result = await _mediator.Send(query, cancellationToken);
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetBlogById(int id, CancellationToken cancellationToken)
-        {
-            var query = new GetBlogByIdQuery(id);
-            var result = await _mediator.Send(query, cancellationToken);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog(
+        [FromBody] BlogRequestDto requestDto,
+        CancellationToken cancellationToken
+    )
+    {
+        var command = new CreateBlogCommand(requestDto);
+        var result = await _mediator.Send(command, cancellationToken);
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog(
-            [FromBody] BlogRequestDto requestDto,
-            CancellationToken cancellationToken
-        )
-        {
-            var command = new CreateBlogCommand(requestDto);
-            var result = await _mediator.Send(command, cancellationToken);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBlog(
+        [FromBody] BlogRequestDto requestDto,
+        int id,
+        CancellationToken cancellationToken
+    )
+    {
+        var command = new UpdateBlogCommand(requestDto, id);
+        var result = await _mediator.Send(command, cancellationToken);
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateBlog(
-            [FromBody] BlogRequestDto requestDto,
-            int id,
-            CancellationToken cancellationToken
-        )
-        {
-            var command = new UpdateBlogCommand(requestDto, id);
-            var result = await _mediator.Send(command, cancellationToken);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchBlog(
+        [FromBody] BlogRequestDto requestDto,
+        int id,
+        CancellationToken cancellationToken
+    )
+    {
+        var command = new PatchBlogCommand(requestDto, id);
+        var result = await _mediator.Send(command, cancellationToken);
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchBlog([FromBody] BlogRequestDto requestDto, int id, CancellationToken cancellationToken)
-        {
-            var command = new PatchBlogCommand(requestDto, id);
-            var result = await _mediator.Send(command, cancellationToken);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBlog(int id, CancellationToken cancellationToken)
+    {
+        var command = new DeleteBlogCommand(id);
+        var result = await _mediator.Send(command, cancellationToken);
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBlog(int id, CancellationToken cancellationToken)
-        {
-            var command = new DeleteBlogCommand(id);
-            var result = await _mediator.Send(command, cancellationToken);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
