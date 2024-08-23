4e7ab97ede37d4143edc6f383bb94aa236dd2380
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:32:54 +0630

    -

---
+++ DotNet8.Architectures.Hexagonal.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using DotNet8.Architectures.Clean.Domain.Features.Blog;
+using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Hexagonal.Domain.Features.Blog;
+using DotNet8.Architectures.Utils;
+using DotNet8.Architectures.Utils.Resources;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.Hexagonal.Application.Features.Blog.PatchBlog
+{
+    public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogDto>>
+    {
+        private readonly IBlogPort _blogPort;
+
+        public PatchBlogCommandHandler(IBlogPort blogPort)
+        {
+            _blogPort = blogPort;
+        }
+
+        public async Task<Result<BlogDto>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+        {
+            Result<BlogDto> result;
+
+            if (request.BlogId <= 0)
+            {
+                result = Result<BlogDto>.Failure(MessageResource.InvalidId);
+                goto result;
+            }
+
+            result = await _blogPort.PatchBlogAsync(request.BlogRequestDto, request.BlogId, cancellationToken);
+
+        result:
+            return result;
+        }
+    }
+}
