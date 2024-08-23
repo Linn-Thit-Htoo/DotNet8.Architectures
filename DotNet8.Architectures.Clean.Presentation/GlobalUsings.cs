cb2a846e24a93686dfee33dbab15fe5496524a5c
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:56:10 +0630

    [ Added ] Global Usings

---
+++ DotNet8.Architectures.Clean.Presentation/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8.Architectures.Clean.Domain.Features.Blog;
+global using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
+global using Microsoft.EntityFrameworkCore;
