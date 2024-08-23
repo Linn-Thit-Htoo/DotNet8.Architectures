2fe32f7c4a2e201f7a996b495730b5f9f53d93b2
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:35:43 +0630

    -

---
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommand.cs
@@ -0,0 +1,24 @@
+﻿using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.PatchBlog
+{
+    public class PatchBlogCommand : IRequest<Result<BlogDto>>
+    {
+        public BlogRequestDto BlogRequestDto { get; set; }
+
+        public int BlogId { get; set; }
+
+        public PatchBlogCommand(BlogRequestDto blogRequestDto, int blogId)
+        {
+            BlogRequestDto = blogRequestDto;
+            BlogId = blogId;
+        }
+    }
+}
---
+++ DotNet8.Architectures.ModularMonolithic.Modules.Application/Features/Blog/PatchBlog/PatchBlogCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.ModularMonolithic.Modules.Domain.Features.Blog;
+using DotNet8.Architectures.Utils;
+using DotNet8.Architectures.Utils.Resources;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.PatchBlog
+{
+    public class PatchBlogCommandHandler : IRequestHandler<PatchBlogCommand, Result<BlogDto>>
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public PatchBlogCommandHandler(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<Result<BlogDto>> Handle(PatchBlogCommand request, CancellationToken cancellationToken)
+        {
+            Result<BlogDto> result;
+
+            if (request.BlogId <= 0)
+            {
+                result = Result<BlogDto>.Failure(MessageResource.InvalidId);
+                goto result;
+            }
+
+            result = await _blogRepository.PatchBlogAsync(request.BlogRequestDto, request.BlogId, cancellationToken);
+
+        result:
+            return result;
+        }
+    }
+}
