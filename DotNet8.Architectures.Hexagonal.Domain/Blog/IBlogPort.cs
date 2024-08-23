f084466ce4665e0426503648920174588036f519
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 18:17:58 +0630

    -

---
+++ DotNet8.Architectures.Hexagonal.Domain/Blog/IBlogPort.cs
@@ -0,0 +1,35 @@
+﻿using DotNet8.Architectures.DTOs.Features.Blog;
+using DotNet8.Architectures.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.Hexagonal.Domain.Blog
+{
+    public interface IBlogPort
+    {
+        Task<Result<BlogListDtoV1>> GetBlogsAsync(
+            int pageNo,
+            int pageSize,
+            CancellationToken cancellationToken
+        );
+        Task<Result<BlogDto>> GetBlogByIdAsync(int id, CancellationToken cancellationToken);
+        Task<Result<BlogDto>> AddBlogAsync(
+            BlogRequestDto blogRequest,
+            CancellationToken cancellationToken
+        );
+        Task<Result<BlogDto>> UpdateBlogAsync(
+            BlogRequestDto requestDto,
+            int id,
+            CancellationToken cancellationToken
+        );
+        Task<Result<BlogDto>> PatchBlogAsync(
+            BlogRequestDto requestDto,
+            int id,
+            CancellationToken cancellationToken
+        );
+        Task<Result<BlogDto>> DeleteBlogAsync(int id, CancellationToken cancellationToken);
+    }
+}
---
+++ DotNet8.Architectures.Hexagonal.Infrastructure/Blog/BlogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.Architectures.Hexagonal.Infrastructure.Blog
+{
+    public class BlogDbContext
+    {
+    }
+}
