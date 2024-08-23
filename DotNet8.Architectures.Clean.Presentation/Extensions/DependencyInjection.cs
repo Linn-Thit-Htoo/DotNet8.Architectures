17bc3cfcd70a2ed6f076f6d35818715217971293
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:57:02 +0630

    Format Code

--- DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
@@ -2,19 +2,28 @@
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        return services.AddDbContextService(builder)
-            .AddRepositoryService();
+        return services.AddDbContextService(builder).AddRepositoryService();
     }
 
-    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        builder.Services.AddDbContext<BlogDbContext>(opt =>
-        {
-            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+        builder.Services.AddDbContext<BlogDbContext>(
+            opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            },
+            ServiceLifetime.Transient,
+            ServiceLifetime.Transient
+        );
 
         return services;
     }
