fd10b231cda517096180364765eef2f9f21a0bd5
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 22:16:19 +0630

    -

---
+++ DotNet8.Architectures.ModularMonolithic.Modules.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using DotNet8.Architectures.DbService;
+global using DotNet8.Architectures.DTOs.Features.Blog;
+global using DotNet8.Architectures.DTOs.Features.PageSetting;
+global using DotNet8.Architectures.Extensions;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Domain.Features.Blog;
+global using DotNet8.Architectures.Shared;
+global using DotNet8.Architectures.Utils;
+global using Microsoft.EntityFrameworkCore;
---
+++ DotNet8.Architectures.ModularMonolithic.Modules.Presentation/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives
+
+global using DotNet8.Architectures.DTOs.Features.Blog;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.CreateBlog;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.DeleteBlog;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogById;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.GetBlogList;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.PatchBlog;
+global using DotNet8.Architectures.ModularMonolithic.Modules.Application.Features.Blog.UpdateBlog;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
