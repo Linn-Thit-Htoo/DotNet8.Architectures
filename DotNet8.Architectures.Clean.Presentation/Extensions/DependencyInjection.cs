26135f94883e0eb919f88fc928383757f399dbd4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:56:11 +0630

    [ Added ] Global Usings

--- DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
+++ DotNet8.Architectures.Clean.Presentation/Extensions/DependencyInjection.cs
@@ -1,31 +1,26 @@
-﻿using DotNet8.Architectures.Clean.Domain.Features.Blog;
-using DotNet8.Architectures.Clean.Infrastructure.Features.Blog;
-using Microsoft.EntityFrameworkCore;
+﻿namespace DotNet8.Architectures.Clean.Presentation.Extensions;
 
-namespace DotNet8.Architectures.Clean.Presentation.Extensions
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder)
-                .AddRepositoryService();
-        }
+        return services.AddDbContextService(builder)
+            .AddRepositoryService();
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<BlogDbContext>(opt =>
         {
-            builder.Services.AddDbContext<BlogDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            return services.AddScoped<IBlogRepository, BlogRepository>();
-        }
+    private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+    {
+        return services.AddScoped<IBlogRepository, BlogRepository>();
     }
 }
