93b38b4b83f45ced007c73deac6783b48c370587
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-23 21:57:55 +0630

    [ Complete ] Formatting Clean Architecture

--- DotNet8.Architectures.Clean.Presentation/Program.cs
+++ DotNet8.Architectures.Clean.Presentation/Program.cs
@@ -1,10 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +10,6 @@ builder.Services.AddMediatRService();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
