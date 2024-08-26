d97ca9eee9a72b208c3f15c83f14e02cba03cc44
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-26 22:34:55 +0630

    -

--- DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
+++ DotNet8.Architectures.Clean.Infrastructure/Features/Blog/BlogRepository.cs
@@ -127,6 +127,8 @@ public class BlogRepository : IBlogRepository
 
     #endregion
 
+    #region Update Blog Async
+
     public async Task<Result<BlogDto>> UpdateBlogAsync(
         BlogRequestDto requestDto,
         int id,
@@ -163,6 +165,8 @@ public class BlogRepository : IBlogRepository
     result:
         return result;
     }
+
+    #endregion
 
     public async Task<Result<BlogDto>> PatchBlogAsync(
         BlogRequestDto requestDto,
